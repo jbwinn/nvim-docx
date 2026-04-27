c801b7d7756cfd2b43e082df660ce2fd5c349c7c
Jaden Winn <jaden.winn@gmail.com>
2026-04-27 12:27:44 -0700

    Fix file events (BufReadCmd/BufWriteCmd) and block zipPlugin for docx

--- plugin/nvim-docx.lua
+++ plugin/nvim-docx.lua
@@ -3,11 +3,14 @@ if vim.g.loaded_nvim_docx == 1 then
 end
 vim.g.loaded_nvim_docx = 1
 
+-- Prevent Neovim's default zipPlugin from taking over .docx files
+vim.g.zipPlugin_ext = '*.zip,*.jar,*.xpi,*.ja,*.war,*.ear,*.cel,*.apk,*.zpak,*.dirac,*.bz2,*.gz,*.xz,*.zst,*.tar,*.tgz,*.tbz,*.txz,*.tzst,*.docm,*.dotm,*.dotx,*.ear,*.epub,*.gox,*.idml,*.ipa,*.jar,*.kmz,*.nupkg,*.oxt,*.pk3,*.ps3,*.scz,*.smd,*.swz,*.vtx,*.war,*.xpi,*.xps'
+
 -- Create autocommand group
 local augroup = vim.api.nvim_create_augroup("NvimDocx", { clear = true })
 
 -- Handle opening .docx files
-vim.api.nvim_create_autocmd("BufReadPre", {
+vim.api.nvim_create_autocmd("BufReadCmd", {
   group = augroup,
   pattern = "*.docx",
   callback = function()
@@ -16,8 +19,9 @@ vim.api.nvim_create_autocmd("BufReadPre", {
 })
 
 -- Handle saving converted markdown back to .docx
-vim.api.nvim_create_autocmd("BufWritePost", {
+vim.api.nvim_create_autocmd("BufWriteCmd", {
   group = augroup,
+  pattern = "*.docx",
   callback = function()
     require("nvim-docx").handle_docx_save()
   end,
